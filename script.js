4f3dd43e94f644f840effee8008c21986ee2509f
Devil841 <102499062+Devil841@users.noreply.github.com>
2022-11-23 12:12:48 +0530

    Update script.js

--- script.js
+++ script.js
@@ -7,43 +7,21 @@ let num = document.getElementById("num");
 let result = document.getElementById("result");
 let gst = document.getElementById("gst");
 
-function print() {
-  result.innerText =
-    Number(amount.value) +
-    (Number(amount.value) * Number(gst.value)) / 100 +
-    (Number(tip.value) * Number(tip.value)) / 100;
-}
-
-function print() {
-  let result = document.getElementById("result");
-  let amount = parseInt(document.getElementById("bill").value);
-  result.innerText = amount;
+const print=()=>{
+   result.innerText =
+   Math.ceil((Number(amount.value) + (Number(amount.value)*Number(gst.value)/100) + (Number(amount.value) * Number(tip.value) / 100))/person);
 }
 
 plus.onclick = () => {
-  amount = parseInt(document.getElementById("bill").value);
-  tip = parseInt(document.getElementById("tip").value);
-  gst = parseInt(document.getElementById("gst").value);
-  console.log(amount);
-  person++;
-  num.innerText = person;
-  result.innerText = Math.floor(
-    (amount + (amount * gst) / 100 + amount * (tip / 100)) / person
-  );
+   person++;
+   num.innerText = person;
+   print();
 };
 
 min.onclick = () => {
-  if (person > 1) {
-    person--;
-    num.innerText = person;
-    result.innerText = Math.floor(
-      (amount + (amount * gst) / 100 + amount * (tip / 100)) / person
-    );
-  }
+   if (person > 1) {
+      person--;
+      num.innerText = person;
+      print();
+   }
 };
-function print() {
-  result.innerText =
-    Number(amount.value) +
-    (Number(amount.value) * Number(gst.value)) / 100 +
-    (Number(tip.value) * Number(tip.value)) / 100;
-}
